6e0246ebde46e19690a4ef4a16657eb385b0d02e
piloteer44 [Emil] <emilandersen@live.com>
2020-10-20 15:27:03 +0200

    Created turtle screens for respective response
    Co-Authored-By: Albin <54792680+AlbinN01@users.noreply.github.com>
    Co-Authored-By: Erik Reider <35975961+KermitTheFrogerino@users.noreply.github.com>

---
+++ python/turtleTEST.py
@@ -0,0 +1,31 @@
+import turtle
+import tkinter as TK
+
+turtle.getscreen()
+turtle.hideturtle()
+
+def wrongTime():
+    turtle.bgcolor("red")
+    turtle.color("white")
+    turtle.write("Du har inte lunch nu!", move=False, align="center", font=("Arial", 52, "bold"))
+    turtle.penup()
+    turtle.goto(0,-150)
+    turtle.pendown()
+    turtle.write("Din lunch är mellan: 12.10-12.30.", move=False, align="center", font=("Arial", 28, "bold"))
+
+def onTime():
+    turtle.bgcolor("green")
+    turtle.color("white")
+    turtle.write("Välkommen in!", move=False, align="center", font=("Arial", 52, "bold"))
+
+def invalidTag():
+    turtle.bgcolor("red")
+    turtle.color("white")
+    turtle.write("Din tagg är ogiltig, använd en giltig tagg!", move=False, align="center", font=("Arial", 52, "bold"))
+    turtle.penup()
+    turtle.goto(0,-150)
+    turtle.pendown()
+    turtle.write("Se till att skanna taggen separat från andra läsbara kort.", move=False, align="center", font=("Arial", 28, "bold"))
+
+
+TK.mainloop()
