ba5c8210ef702a44b9bdf6074676061584c56aa2
piloteer44 [Emil] <emilandersen@live.com>
2020-10-21 09:51:42 +0200

    UI, made into class and added idlescreen+fullscreen
    Co-Authored-By: Erik Reider <35975961+KermitTheFrogerino@users.noreply.github.com>
    Co-Authored-By: Albin <54792680+AlbinN01@users.noreply.github.com>

--- python/turtleTEST.py
+++ python/turtleTEST.py
@@ -1,31 +1,53 @@
 import turtle
 import tkinter as TK
 
-turtle.getscreen()
-turtle.hideturtle()
 
-def onTime():
-    turtle.bgcolor("green")
-    turtle.color("white")
-    turtle.write("Välkommen in!", move=False, align="center", font=("Arial", 52, "bold"))
+class UI():
 
-def wrongTime():
-    turtle.bgcolor("red")
-    turtle.color("white")
-    turtle.write("Du har inte lunch nu!", move=False, align="center", font=("Arial", 52, "bold"))
-    turtle.penup()
-    turtle.goto(0,-150)
-    turtle.pendown()
-    turtle.write("Din lunch är mellan: 12.10-12.30.", move=False, align="center", font=("Arial", 28, "bold"))
+    def __init__(self):
+        root = (turtle.getturtle()
+            ._screen
+            .getcanvas()
+            .winfo_toplevel())
+        root.attributes('-fullscreen', True)    
+        turtle.getscreen()
+        turtle.hideturtle() 
+        turtle.speed(20)
 
-def invalidTag():
-    turtle.bgcolor("red")
-    turtle.color("white")
-    turtle.write("Din tagg är ogiltig, använd en giltig tagg!", move=False, align="center", font=("Arial", 52, "bold"))
-    turtle.penup()
-    turtle.goto(0,-150)
-    turtle.pendown()
-    turtle.write("Se till att skanna taggen separat från andra läsbara kort.", move=False, align="center", font=("Arial", 28, "bold"))
+    def idleScreen(self):
+        turtle.bgcolor("white")
+        turtle.color("black")
+        turtle.write("Vänligen skanna din tagg.", move=False, align="center", font=("Arial", 52, "bold"))
+        turtle.penup()
+        turtle.goto(0,-350)
+        turtle.pendown()
+        turtle.showturtle()
+        turtle.right(90)
+        turtle.shapesize(16)
 
+    def onTime(self):
+        turtle.bgcolor("green")
+        turtle.color("white")
+        turtle.write("Välkommen in!", move=False, align="center", font=("Arial", 52, "bold"))
+
+    def wrongTime(self):
+        turtle.bgcolor("red")
+        turtle.color("white")
+        turtle.write("Du har inte lunch nu!", move=False, align="center", font=("Arial", 52, "bold"))
+        turtle.penup()
+        turtle.goto(0,-150)
+        turtle.pendown()
+        turtle.write("Din lunch är mellan: 12.10-12.30.", move=False, align="center", font=("Arial", 28, "bold"))
+
+    def invalidTag(self):
+        turtle.bgcolor("red")
+        turtle.color("white")
+        turtle.write("Din tagg är ogiltig, använd en giltig tagg!", move=False, align="center", font=("Arial", 52, "bold"))
+        turtle.penup()
+        turtle.goto(0,-150)
+        turtle.pendown()
+        turtle.write("Se till att skanna taggen separat från andra läsbara kort.", move=False, align="center", font=("Arial", 28, "bold"))
+
+UI().idleScreen()
 
 TK.mainloop()
